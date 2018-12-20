3eaf502b3406999508d65bd1041617b1b22674b5
ikostan <igorkostan@gmail.com>
2018-12-19 19:28:37 -0800

    Mars Lander - Episode 1
    Mars Lander - Episode 1 is solved

--- CLASSIC_PUZZLE_EASY/MARS_LANDER_EPISODE_1.py
+++ CLASSIC_PUZZLE_EASY/MARS_LANDER_EPISODE_1.py
@@ -23,6 +23,8 @@ Note that it may look like a difficult problem, but in reality the problem
 is easy to solve. This puzzle is the first level among three, therefore,
 we need to present you some controls you won't need in order to complete this first level.
 
+For this level, you only need to control the thrust power: the tilt angle should be 0.
+
 The game simulates a free fall without atmosphere. Gravity on Mars is 3.711 m/s² . For a thrust power of X, a push force equivalent to X m/s² is generated and X liters of fuel are consumed. As such, a thrust power of 4 in an almost vertical position is needed to compensate for the gravity on Mars.
 
 For a landing to be successful, the ship must:
@@ -39,4 +41,38 @@ you only need your vertical landing speed to be between 0 and 40m/s – your hor
 As the shuttle falls, the vertical speed is negative.
 As the shuttle flies upward, the vertical speed is positive.
 '''
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+surface_n = int(input())  # the number of points used to draw the surface of Mars.
+for i in range(surface_n):
+    # land_x: X coordinate of a surface point. (0 to 6999)
+    # land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+    land_x, land_y = [int(j) for j in input().split()]
+
+# game loop
+while True:
+    # h_speed: the horizontal speed (in m/s), can be negative.
+    # v_speed: the vertical speed (in m/s), can be negative.
+    # fuel: the quantity of remaining fuel in liters.
+    # rotate: the rotation angle in degrees (-90 to 90).
+    # power: the thrust power (0 to 4).
+    x, y, h_speed, v_speed, fuel, rotate, power = [int(i) for i in input().split()]
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+    # print("Trust power: " + str(power), file=sys.stderr)
+
+    if v_speed < -40 and power < 4:
+        power = power + 1
+
+    if v_speed > 0 and power > 0:
+        power = power - 1
+
+    # 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+    print(rotate, power)
 
