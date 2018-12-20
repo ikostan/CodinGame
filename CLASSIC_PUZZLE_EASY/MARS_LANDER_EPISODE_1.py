88903127ea9870aa196c0d86bc3a5431e7c28960
ikostan <igorkostan@gmail.com>
2018-12-19 19:37:05 -0800

    Comments
    some minor changes

--- CLASSIC_PUZZLE_EASY/MARS_LANDER_EPISODE_1.py
+++ CLASSIC_PUZZLE_EASY/MARS_LANDER_EPISODE_1.py
@@ -51,7 +51,9 @@ import math
 surface_n = int(input())  # the number of points used to draw the surface of Mars.
 for i in range(surface_n):
     # land_x: X coordinate of a surface point. (0 to 6999)
-    # land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+    # land_y: Y coordinate of a surface point.
+    # By linking all the points together in a sequential fashion,
+    # you form the surface of Mars.
     land_x, land_y = [int(j) for j in input().split()]
 
 # game loop
@@ -73,6 +75,7 @@ while True:
     if v_speed > 0 and power > 0:
         power = power - 1
 
-    # 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+    # 2 integers: rotate power. rotate is the desired rotation angle
+    # (should be 0 for level 1), power is the desired thrust power (0 to 4).
     print(rotate, power)
 
