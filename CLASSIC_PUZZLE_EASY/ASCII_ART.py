d14cc7a9c9556f8818c2a91fed668f50d02b910e
ikostan <igorkostan@gmail.com>
2018-12-21 12:44:01 -0800

    ASCII ART
    ASCII ART > start a new problem

---
+++ CLASSIC_PUZZLE_EASY/ASCII_ART.py
@@ -0,0 +1,23 @@
+'''
+
+ASCII ART
+Source: https://www.codingame.com/training/easy/ascii-art
+
+WHAT WILL I LEARN?
+LoopsArraysStrings
+Solving this puzzle teaches how to manage strings and array arithmetics.
+
+You'll know how to split a string into separate parts and concatenate them into a new one.
+
+You can use indexes of arrays
+
+External resources LoopsStringsLet's Play ASCII Art
+
+STATEMENT
+The goal of the problem is to simulate an old airport terminal display:
+your program must display a line of text in ASCII art.
+
+You have to split strings, store them and recreate others.
+You can use data structures like arrays or hash tables.
+
+'''
