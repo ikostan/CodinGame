fb0474ed2a8ce60c4d9867d6a11698e7ae7218b3
ikostan <igorkostan@gmail.com>
2018-12-21 16:17:26 -0800

    ASCII Art
    Done

--- CLASSIC_PUZZLE_EASY/ASCII_ART.py
+++ CLASSIC_PUZZLE_EASY/ASCII_ART.py
@@ -50,3 +50,40 @@ Constraints
 0 < N < 200
 
 '''
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ?'
+
+l = int(input())
+h = int(input())
+t = input()
+print(t, file=sys.stderr)
+
+rows = []
+
+for i in range(h):
+    row = input()
+    # print(row, file=sys.stderr)
+    rows.append(row)
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+
+for row in rows:
+    result = ''
+    for itemT in t:
+        # print(itemT, file=sys.stderr)
+        if itemT.upper() in chars:
+            result += row[(chars.index(itemT.upper()) * l):(chars.index(itemT.upper()) * l) + l]
+            # print(itemT + ": " + result, file=sys.stderr)
+        else:
+            itemT = '?'
+            result += row[(chars.index(itemT.upper()) * l):(chars.index(itemT.upper()) * l) + l]
+    print(result)
