8082d7cf013a70094bd0bcfb00d28999600ef661
ikostan <igorkostan@gmail.com>
2018-12-22 12:22:04 -0800

    Defibrillators
    Defibrillators > Final version works

--- CLASSIC_PUZZLE_EASY/DEFIBRILLATORS.py
+++ CLASSIC_PUZZLE_EASY/DEFIBRILLATORS.py
@@ -45,5 +45,72 @@ Beware: the decimal numbers use the comma (,) as decimal separator.
 Remember to turn the comma (,) into dot (.) if necessary in order
 to use the data in your program.
 
-
 '''
+
+import sys
+import math
+
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+def calcDistance(longUser, longDefib, latitUser, latitDefib):
+    longDefib = float(longDefib)
+    latitDefib = float(latitDefib)
+    x = (longDefib - longUser) * math.cos((latitUser + latitDefib) / 2.)
+    y = (latitDefib - latitUser)
+
+    # Note: In this formula, the latitudes and longitudes are expressed in radians.
+    # 6371 corresponds to the radius of the earth in km.
+
+    distance = math.sqrt(math.pow(x, 2) + math.pow(y, 2)) * 6371.
+
+    return distance
+
+
+lon = input()
+# print("Lon: " + str(lon), file=sys.stderr)
+
+lat = input()
+# print("Lat: " + str(lat), file=sys.stderr)
+
+userLocation = {'lon': float(lon.replace(',', '.')), 'lat': float(lat.replace(',', '.'))}
+# print(userLocation, file=sys.stderr)
+
+n = int(input())
+# print("N: " + str(n), file=sys.stderr)
+
+defibLocation = []
+
+for i in range(n):
+    defib = input()
+    # print(defib, file=sys.stderr)
+    defTempData = defib.split(';')
+    defibLocation.append(defTempData)
+
+# print(defibLocation, file=sys.stderr)
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+distances = []
+
+# print(defibLocation[0][5].replace(',','.'), file=sys.stderr)
+
+index = 0
+for defibItem in defibLocation:
+    # print("index: " + str(index), file=sys.stderr)
+    distances.append(calcDistance(userLocation['lon'], defibLocation[index][4].replace(',', '.'), userLocation['lat'],
+                                  defibLocation[index][5].replace(',', '.')))
+    index += 1
+
+# rint(distances, file=sys.stderr)
+
+answer = 'N/A'
+if len(distances) > 0:
+    minDist = min(distances)
+    minIndx = distances.index(minDist)
+    answer = defibLocation[minIndx][1]
+
+print(answer)
