3ef63191a55a49b9b981b09026f922aa55d6e742
ikostan <igorkostan@gmail.com>
2018-12-22 11:40:05 -0800

    Defibrillators
    Starting a new puzzle

---
+++ CLASSIC_PUZZLE_EASY/DEFIBRILLATORS.py
@@ -0,0 +1,49 @@
+'''
+
+DEFIBRILLATORS
+Source: https://www.codingame.com/training/easy/defibrillators
+
+WHAT WILL I LEARN?
+LoopsDistancesTrigonometry
+This puzzle makes you split a string into substrings,
+replace a character of a string and convert it into floats.
+You also have to convert degrees into radians and use mathematical
+operations like square root or cosinus.
+
+Finally you have to apply multiple computations (addition, multiplication...)
+on floating numbers.
+
+STATEMENT
+The goal of this exercise is to make you find the closest point to given
+geographic coordinates (latitude and longitude). Your program must print
+the associated location name.
+
+In this puzzle you will play a lot with conversion (degree to radian,
+coordinates to distance, string to float).
+
+The Goal
+The city of Montpellier has equipped its streets with defibrillators to
+help save victims of cardiac arrests. The data corresponding to the position
+of all defibrillators is available online.
+
+Based on the data we provide in the tests, write a program that will
+allow users to find the defibrillator nearest to their location using their mobile phone.
+
+Rules
+The input data you require for your program is provided in text format.
+This data is comprised of lines, each of which represents a defibrillator.
+Each defibrillator is represented by the following fields:
+A number identifying the defibrillator
+Name
+Address
+Contact Phone number
+Longitude (degrees)
+Latitude (degrees)
+These fields are separated by a semicolon (;).
+
+Beware: the decimal numbers use the comma (,) as decimal separator.
+Remember to turn the comma (,) into dot (.) if necessary in order
+to use the data in your program.
+
+
+'''
